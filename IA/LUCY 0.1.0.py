7b40107517e1d8d88eadf5de846fa7d56d646224
João Prado <joao.victor0705@gmail.com>
2024-03-24 19:03:59 -0300

    a prototipe of a game central

--- IA/LUCY 0.1.0.py
+++ IA/LUCY 0.1.0.py
@@ -1,6 +1,7 @@
 import random
+import PPT_in_AI as pedraPTesoura
 
-# Dicionário de perguntas e respostas
+
 conversas = {
     "Olá": ["Olá!", "Oie!", "Oi!", "Olá, como posso ajudar?"],
     "Como você está?": ["Estou bem, obrigado por perguntar!", "Estou ótimo!", "Nunca estive melhor!"],
@@ -8,23 +9,38 @@ conversas = {
     "Qual é a sua idade?": ["Eu sou uma Inteligencia Artificial, eu não tenho idade.", "Idade é uma noção humana, não se aplica a mim."],
     "O que você pode fazer?": ["Posso responder perguntas, conversar e fornecer informações.", "Estou aqui para ajudar com o que precisar."],
     "Obrigado": ["De nada!", "Não há de quê!", "Estou sempre aqui para ajudar!"],
-    "Qual a sua versão?": ["No momento me apresento na versão 0.0.1, e ainda estou em fase de testes", "Sou apenas um protótipo"]
+    "Qual a sua versão?": ["No momento me apresento na versão 0.1.0, e ainda estou em fase de testes", "Sou apenas um protótipo", "Ainda não sou uma Inteligência Artificial efetivamente, mas meu dev esta trabalhando duro para isso ser uma realidade em breve!"],
+    "Quais são as novidades da nova versão?": ["Agora tenho um protótipo de central de jogos, me pergunte 'Quero jogar um jogo' para testar!"]
+}
+externos = {
+    "Quero jogar um jogo": []
 }
 
-# Função para responder à pergunta do usuário
 def responder(pergunta):
     resposta_padrao = "Desculpe, não entendi sua pergunta. Poderia reformular?"
     if pergunta in conversas:
         return random.choice(conversas[pergunta])
+    elif pergunta in externos:
+        return jogos()
     else:
         return resposta_padrao
 
-# Loop do chat
+def jogos():
+    jogo = input("Claro! Qual gostaria de jogar? ")
+    
+    not_yet = "Desculpe, ainda nao tenho esse jogo"
+    if jogo == 'Pedra, papel ou tesoura':
+        return pedraPTesoura.PPT()
+    else:
+        return not_yet
+    
+
+
 print("Bem-vindo à LUCY. Digite 'sair' para encerrar o chat.")
 while True:
     pergunta = input("Você: ")
-    if pergunta.lower() == 'sair':
-        print("Chat encerrado, até mais")
+    if pergunta.lower() == 'sair' or pergunta.lower() == 'tchau':
+        print("Chat encerrado, até mais!")
         break
     resposta = responder(pergunta)
     print("LUCY:", resposta)
---
+++ IA/PPT_in_AI.py
@@ -0,0 +1,106 @@
+#Thats a "def" version of the original Jokenpo game that i made
+
+def PPT():
+    import random
+    import math
+    import time
+    
+    jogo = ['pedra', 'papel', 'tesoura']
+    pontuacao_IA = 0
+    pontuacao_jogador = 0
+    partida = 0
+    lag = 0
+
+    print("Olá, meu nome é L.U.C.Y")
+    time.sleep(1)
+    while True:
+        destino = input("Vamos jogar um jogo (sim ou nao)? ")
+        if destino == 'sim':
+            print("Que bom >:)")
+            break
+        elif destino == 'nao':
+            lag += 1
+            time.sleep(lag)
+            print("Sempre que o vosso destino for negado, mais a verdade será atrasada")
+            time.sleep(lag)
+        elif destino == 'silêncio vadia':
+            print("silêncio você ooou cabeça de alpiste")
+        else:
+            print("afirmação inválida hehehe")
+
+
+    while True:
+        rodadas = int(input("melhor de... "))
+        if rodadas % 2 != 0:
+            break
+        else:
+            print("Apenas valores Impares, cacete")
+    valor_dividido = rodadas / 2
+    valor_melhor_de = math.ceil(valor_dividido)
+
+    while pontuacao_IA < valor_melhor_de and pontuacao_jogador < valor_melhor_de:
+        if partida >= rodadas:
+            break
+
+        random.shuffle(jogo)
+        escolha_IA = random.choice(jogo)
+        usuario = str(input("Escolha UM (pedra, papel ou tesoura): "))
+        usuario = usuario.lower()
+        usuario = usuario.strip()
+
+        if (usuario == 'sair'):
+            break
+
+        elif usuario not in jogo:
+            print("O dado inserido não é válido neste jogo ")
+            continue
+
+        #IA ganha
+        elif (escolha_IA == 'pedra' and usuario == 'tesoura') or (escolha_IA == 'papel' and usuario == 'pedra') or (escolha_IA == 'tesoura' and usuario == 'papel'):
+            pontuacao_IA += 1
+            partida += 1
+            print("que pena, você escolheu", escolha_IA, "e eu escolhi", usuario, ", ponto para mim hehehe")
+            if partida != rodadas and pontuacao_IA != valor_melhor_de and pontuacao_jogador != valor_melhor_de:
+                print("Eu: ", pontuacao_IA, "Você: ", pontuacao_jogador)
+
+        #Jogador ganha
+        elif (escolha_IA == 'tesoura' and usuario == 'pedra') or (escolha_IA == 'pedra' and usuario == 'papel') or (escolha_IA == 'papel' and usuario == 'tesoura'):
+            pontuacao_jogador += 1
+            partida += 1
+            print("droga, você escolheu", usuario, "e eu escolhi",escolha_IA, ", ponto para você D:<")
+            if partida != rodadas and pontuacao_IA != valor_melhor_de and pontuacao_jogador != valor_melhor_de:
+                print("Eu: ", pontuacao_IA, "Você: ", pontuacao_jogador)
+
+        #Empate
+        elif (escolha_IA == usuario):
+            print("Empate")
+            print("Ninguem ganha, ninguem perde")
+
+        else:
+            print("Não foi possível achar um resultado")
+
+        if partida != rodadas and pontuacao_IA != valor_melhor_de and pontuacao_jogador != valor_melhor_de:
+            print("vamos de novo")
+
+
+    if (pontuacao_IA == valor_melhor_de):
+        print("")
+        print("fim de jogo!")
+        print("perdeu troxa")
+        print("Eu: ", pontuacao_IA, "Você: ", pontuacao_jogador)
+
+    elif (pontuacao_jogador == valor_melhor_de):
+        print("")
+        print("fim de jogo!")
+        print("claramente foi sorte")
+        print("Eu: ", pontuacao_IA, "Você: ", pontuacao_jogador)
+
+    else:
+        print("O jogo NUNCA termina")
+        
+    return "Até o próximo jogo!"
+
+
+
+#deixar com uma linguagem mais amigável
+#fazer alterações necessárias para melhorar o funcionamento com a IA
