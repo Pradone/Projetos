149346e0c585c4f719181b95987b39adc5e3d389
João Prado <joao.victor0705@gmail.com>
2024-04-01 17:28:39 -0300

    Tamagochi in Java

---
+++ Projetinhos/Tamagoshi/src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String action;
+        String witch;
+        
+        System.out.println("Adicione o nome do primeiro tamagochi ");
+        String name1 = input.next();
+        System.out.println("Adicione o nome do segundo tamagochi ");
+        String name2 = input.next();
+        
+        Tamagochi tama1 = new Tamagochi (name1, 1, 100, 100, 100);
+        Tamagochi tama2 = new Tamagochi (name2, 1, 100, 100, 100);
+        
+        do {
+        	tama1.print();
+	        System.out.println();
+	        tama2.print();
+	        
+	        System.out.println();
+	        System.out.println("O que gostaria de fazer?");
+	        action = input.next();
+	        
+	        
+	        if (action.equals("sair")) {
+	        	break;
+	        }else if (action.equals("alimentar")){
+	        	System.out.println("Qual?");
+	        	witch = input.next();
+	        	if(witch.equals("1") | witch.equals(name1)) {
+	        		tama1.eat();
+	        	}else if (witch.equals("2") | witch.equals(name2)) {
+	        		tama2.eat();
+	        	}else if (witch.equals("os 2")) { 
+	        		tama1.eat();
+	        		tama2.eat();
+	        	}
+	        }else if(action.equals("brincar")) {
+	        	System.out.println("Qual?");
+	        	witch = input.next();
+	        	if(witch.equals("1") | witch.equals(name1)) {
+	        		tama1.play();
+	        	}else if (witch.equals("2") | witch.equals(name2)) {
+	        		tama2.play();
+	        	}else if (witch.equals("os 2")) { 
+	        		tama1.play();
+	        		tama2.play();
+	        	}
+	        }
+	        tama1.statusIncrease();
+        	tama2.statusIncrease();
+        }while (true);
+        input.close();
+        
+        System.out.println("Encerrando Programa...");
+        try {
+        	Thread.sleep(1750);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Programa encerrado");
+	}
+
+}
---
+++ Projetinhos/Tamagoshi/src/Tamagochi.java
@@ -0,0 +1,114 @@
+public class Tamagochi {
+
+	private String name;
+	private int age;
+	private double health;
+	private double hapiness;
+	private double hunger;
+	
+	public Tamagochi(String name, int age, double health, double hapiness, double hunger) {
+		this.name = name;
+		this.age = age;
+		this.health = health;
+		this.hapiness = hapiness;
+		this.hunger = hunger;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public double getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public double getHapiness() {
+		return hapiness;
+	}
+	public void setHapiness(int hapiness) {
+		this.hapiness = hapiness;
+	}
+	public double getHunger() {
+		return hunger;
+	}
+	public void setHunger(int hunger) {
+		this.hunger = hunger;
+	}
+	
+	public void hungry() {
+        hunger -= 15;
+        if(hunger < 50) {
+    		System.out.println(name +" está ficando com fome, alimente-o(a)!");
+        }else if(hunger <= 0) {
+        	System.out.println(name +" está com fome, alimente-o(a)!");
+        }
+    }
+	public void sadness() {
+        hapiness -= 15;
+        if(hapiness <= 50) {
+        	System.out.println(name +" está ficando triste, deixe-o(a) feliz!");
+        }else if(hapiness <= 0) {
+        	System.out.println(name +" está triste, deixe-o(a) feliz!");
+        }
+    }
+	public void dying() {
+        health -= 10;
+        if (health <= 0) {
+        	health = 0;
+        	System.out.println(name+" está morto...");
+        }
+    }
+	public void older() {
+		age += 1;
+		if (age >= 10) {
+			System.out.println(name +" está ficando velhinho(a)");
+		}else if (age >= 11) {
+			health -= 25;
+		}
+	}
+	public void play() {
+		hapiness += 35;
+		if (hapiness <= 100) {
+			System.out.println("Você brincou com "+ name +", ele parece estar mais feliz!");
+		}else if (hapiness >= 100) {
+			hapiness = 115;
+        	System.out.println(name +" está bem feliz!");
+        }else if (hapiness <= 0) {
+        	hapiness = 0;
+        	System.out.println(name +" está bem triste!");
+        }
+    }
+	public void eat() {
+		hunger += 35;
+		health += 10;
+		if (hunger <= 100) {
+	        System.out.println(name +" foi alimentado");
+		}else if (hunger >= 100){
+			hunger = 115;
+			System.out.println(name +" está de barriga lotada");
+		}else if (hunger <= 0) {
+			hunger = 0;
+			System.out.println(name +" está está com fome");
+		}
+    }
+	
+	public void statusIncrease() {
+		System.out.println();
+		hungry();
+		sadness();
+		dying();
+		older();
+		System.out.println();
+	}
+	public void print() {
+		System.out.println("Nome: "+ name);
+		System.out.println("Idade: "+ age);
+		System.out.println("Saúde: "+ health);
+		System.out.println("Fome: "+ hunger);
+		System.out.println("Felicidade: "+ hapiness);
+	}
+}
