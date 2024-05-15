8e449bc4e478f805144740dc0af44791d63287e6
João Prado <joao.victor0705@gmail.com>
2024-05-14 22:07:50 -0300

    formating

--- Projetinhos/AutoInputs/AutoClicker.py
+++ Projetinhos/AutoInputs/AutoClicker.py
@@ -4,25 +4,27 @@ import keyboard as key
 import time
 
 stop_autoClick = False
+
+
 def main(pag):
     tittle = ft.Text("Autoclicker", size=25, weight=ft.FontWeight.BOLD)
-    
+
     def start_autoClick(event):
         global stop_autoClick
         enable_text = ft.Text("The autoclicker is ",
-                            size=15,
-                            weight=ft.FontWeight.BOLD)
+                              size=15,
+                              weight=ft.FontWeight.BOLD)
         enable_text2 = ft.Text("enabled",
-                            size=15,
-                            weight=ft.FontWeight.BOLD,
-                            color=ft.colors.GREEN)
-        pag.add(enable_text,enable_text2)
+                               size=15,
+                               weight=ft.FontWeight.BOLD,
+                               color=ft.colors.GREEN)
+        pag.add(enable_text, enable_text2)
         stop_autoClick = False
         while True:
             if key.is_pressed('F4'):
                 time.sleep(0.25)
                 while True:
-                    if key.is_pressed('F4'): 
+                    if key.is_pressed('F4'):
                         time.sleep(0.25)
                         break
                     else:
@@ -40,7 +42,7 @@ def main(pag):
     start_button = ft.ElevatedButton("Start", on_click=start_autoClick, color=ft.colors.GREEN)
     end_button = ft.ElevatedButton("End", on_click=end_autoClick, color=ft.colors.RED)
     buttons = ft.Row([start_button, end_button])
-    
+
     pag.title = "PyClick"
     pag.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     pag.theme_mode = "dark"
@@ -50,7 +52,8 @@ def main(pag):
     pag.window_width = 200
     pag.window_height = 200
     pag.add(tittle, buttons)
+
+
 ft.app(main)
 
-
-#add some additional functions
+# add some additional functions
